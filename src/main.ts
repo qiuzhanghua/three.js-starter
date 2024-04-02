3199a34ed16e18da23d46717a25b4ba362075ec7
qiuzhanghua <qiuzhanghua@icloud.com>
2024-04-03 07:46:55 +0800

    feat: Add AxisHelper

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import Stats from "three/examples/jsm/libs/stats.module";
 import { GUI } from "dat.gui";
 
 const scene = new Three.Scene();
+scene.add(new Three.AxesHelper(5));
 const camera = new Three.PerspectiveCamera(
   75,
   window.innerWidth / window.innerHeight,
@@ -44,10 +45,23 @@ document.body.appendChild(stats.dom);
 
 const gui = new GUI();
 const cubeFolder = gui.addFolder("Cube");
-cubeFolder.add(cube.rotation, "x", 0, Math.PI * 2);
-cubeFolder.add(cube.rotation, "y", 0, Math.PI * 2);
-cubeFolder.add(cube.rotation, "z", 0, Math.PI * 2);
+const cubeRotationFolder = cubeFolder.addFolder("Rotation");
+cubeRotationFolder.add(cube.rotation, "x", 0, Math.PI * 2);
+cubeRotationFolder.add(cube.rotation, "y", 0, Math.PI * 2);
+cubeRotationFolder.add(cube.rotation, "z", 0, Math.PI * 2);
 cubeFolder.open();
+cubeRotationFolder.open();
+const cubePositionFolder = cubeFolder.addFolder("Position");
+cubePositionFolder.add(cube.position, "x", -10, 10, 0.1);
+cubePositionFolder.add(cube.position, "y", -10, 10, 0.1);
+cubePositionFolder.add(cube.position, "z", -10, 10, 0.1);
+cubePositionFolder.open();
+const cubeScaleFolder = cubeFolder.addFolder("Scale");
+cubeScaleFolder.add(cube.scale, "x", 0, 5);
+cubeScaleFolder.add(cube.scale, "y", 0, 5);
+cubeScaleFolder.add(cube.scale, "z", 0, 5);
+cubeScaleFolder.open();
+cubeFolder.add(cube, "visible");
 const cameraFolder = gui.addFolder("Camera");
 cameraFolder.add(camera.position, "z", 0, 10);
 cameraFolder.open();
