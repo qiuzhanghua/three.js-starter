82625afe70c4cad09ae47608a0935f2887f9bcfd
qiuzhanghua <qiuzhanghua@icloud.com>
2024-04-02 18:31:55 +0800

    init

---
+++ src/main.ts
@@ -0,0 +1,43 @@
+import "./style.css";
+import * as Three from "three";
+
+const scene = new Three.Scene();
+const camera = new Three.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000,
+);
+camera.position.z = 2;
+
+const renderer = new Three.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+const geometry = new Three.BoxGeometry();
+const material = new Three.MeshBasicMaterial({
+  color: 0x00ff00,
+  wireframe: true,
+});
+const cube = new Three.Mesh(geometry, material);
+scene.add(cube);
+
+window.addEventListener(
+  "resize",
+  () => {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.render(scene, camera);
+  },
+  false,
+);
+
+function animate() {
+  requestAnimationFrame(animate);
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
+  renderer.render(scene, camera);
+}
+
+animate();
